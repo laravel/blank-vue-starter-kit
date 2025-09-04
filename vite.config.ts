252448c222f2eb9f7719578bc7dc38d5d4731782
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2025-09-04 13:44:27 -0400

    Merge pull request #1 from laravel/ziggy-to-wayfinder
    Ziggy -> Wayfinder

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+import { wayfinder } from '@laravel/vite-plugin-wayfinder';
+import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -18,6 +19,9 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
+        }),
+        wayfinder({
+            formVariants: true,
         }),
     ],
 });
