c3f5b2c6c09186ed8933c7caeff5299e83cf1fbc
Wendell Adriel <me@wendelladriel.com>
2026-01-12 19:16:24 +0000

    Remove quote shared data (#11)
    * Remove quote from Inertia shared data
    
    * Remove not used import

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -4,9 +4,9 @@ export interface Auth {
 
 export type AppPageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
     name: string;
-    quote: { message: string; author: string };
     auth: Auth;
-};
+    [key: string]: unknown;
+}
 
 export interface User {
     id: number;
@@ -16,4 +16,5 @@ export interface User {
     email_verified_at: string | null;
     created_at: string;
     updated_at: string;
+    [key: string]: unknown; // This allows for additional properties...
 }
