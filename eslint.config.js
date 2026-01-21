e87ab91f6303ec586f4b8935d69b9d59ba2d324c
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-01-21 17:31:09 +0000

    Update eslint-plugin-import configuration

--- eslint.config.js
+++ eslint.config.js
@@ -35,7 +35,6 @@ export default defineConfigWithVueTs(
                 'error',
                 {
                     groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-                    'newlines-between': 'never',
                     alphabetize: {
                         order: 'asc',
                         caseInsensitive: true,
